b736b1375d0fb363740db0fd9971c824489797b4
Alicia <alicia.zangger@gmail.com>
2018-05-21 14:29:28 +0200

    Corrected transferBean.message error
    Modified home.xhtml and transferBean.populate method to display a
    confirmation message when populate() is executed. But it's not the case.

--- Anime_J2EE_JAZS/src/main/java/ch/hevs/managedbeans/TransferBean.java
+++ Anime_J2EE_JAZS/src/main/java/ch/hevs/managedbeans/TransferBean.java
@@ -21,16 +21,8 @@ public class TransferBean
 {
     private List<Anime> animes;
     private List<String> animeNames;
-    private AnimeList animeList;
-    
-//    private List<String> sourceAccountDescriptions;
-//    private List<String> destinationAccountDescriptions;
-//    private String sourceAccountDescription;
-//    private String destinationAccountDescription;
-//    private String sourceClientName;
-//    private String destinationClientName;
-//    private String transactionResult;
-//    private int transactionAmount;    
+    private AnimeList animeList;   
+    private String message = "ras";
     
     @PostConstruct
     public void initialize() throws NamingException {
@@ -45,13 +37,6 @@ public class TransferBean
 		for (Anime anime : animes) {
 			this.animeNames.add(anime.getAnimeName());
 		}
-		
-		// initialize account descriptions
-//		this.sourceAccountDescriptions = new ArrayList<String>();
-//		this.destinationAccountDescriptions = new ArrayList<String>();
-//		List<Account> accounts = bank.getAccountListFromClientLastname(clientList.get(0).getLastname());
-//		this.sourceAccountDescriptions.add(accounts.get(0).getDescription());
-//		this.destinationAccountDescriptions.add(accounts.get(0).getDescription());
     }
     
     // animes
@@ -64,91 +49,25 @@ public class TransferBean
     {
     	this.animes = animes;
     }
-   
     
-    // populateDB
-    public void populate()
+    // message
+    public String getMessage()
     {
-    	DatabaseDefaultContent.populate();
+    	return message;
     }
     
-//    // transactionAmount
-//    public int getTransactionAmount () {
-//    	return transactionAmount;
-//    }
-//    public void setTransactionAmount (final int transactionAmount) {
-//    	this.transactionAmount=transactionAmount;
-//    }
-//    
-//    // sourceClientName
-//    public String getSourceClientName () {
-//    	return sourceClientName;
-//    }
-//    public void setSourceClientName (final String sourceClientName) {
-//    	this.sourceClientName=sourceClientName;
-//    }
-//    
-//    // sourceAccountDescriptions
-//    public List<String> getSourceAccountDescriptions () {
-//    	return sourceAccountDescriptions;
-//    }
-//    
-//    // destinationAccountDescriptions
-//    public List<String> getDestinationAccountDescriptions () {
-//    	return destinationAccountDescriptions;
-//    }
-//    
-//    // destinationClientName
-//    public String getDestinationClientName () {
-//    	return destinationClientName;
-//    }
-//    public void setDestinationClientName (final String destinationClientName) {
-//    	this.destinationClientName=destinationClientName;
-//    }
-//    
-//    // transactionResult
-//    public String getTransactionResult () {
-//    	return transactionResult;
-//    }
-//	public void setTransactionResult(String transactionResult) {
-//		this.transactionResult = transactionResult;
-//	}
-//    
-//	// sourceAccountDescription
-//    public String getSourceAccountDescription() {
-//		return sourceAccountDescription;
-//	}
-//	public void setSourceAccountDescription(String sourceAccountDescription) {
-//		this.sourceAccountDescription = sourceAccountDescription;
-//	}
-//
-//	// destinationAccountDescription
-//	public String getDestinationAccountDescription() {
-//		return destinationAccountDescription;
-//	}
-//	public void setDestinationAccountDescription(
-//			String destinationAccountDescription) {
-//		this.destinationAccountDescription = destinationAccountDescription;
-//	}
-
-//	public void updateSourceAccounts(ValueChangeEvent event) {
-//    	this.sourceClientName = (String)event.getNewValue();
-//    	
-//	    List<Account> accounts = bank.getAccountListFromClientLastname(this.sourceClientName);
-//	    this.sourceAccountDescriptions = new ArrayList<String>();
-//		for (Account account : accounts) {
-//			this.sourceAccountDescriptions.add(account.getDescription());
-//		}
-//    }
-//	public void updateDestinationAccounts(ValueChangeEvent event) {
-//    	this.destinationClientName = (String)event.getNewValue();
-//			
-//	    List<Account> accounts = bank.getAccountListFromClientLastname(this.destinationClientName);
-//	    this.destinationAccountDescriptions = new ArrayList<String>();
-//		for (Account account : accounts) {
-//			this.destinationAccountDescriptions.add(account.getDescription());
-//		}
-//    }
+    public void setMessage(String message)
+    {
+    	this.message = message;
+    }   
+    
+    // populateDB
+    public String populate()
+    {
+    	DatabaseDefaultContent.populate();
+    	setMessage("DB populated");
+    	return "home";
+    }
 
     public List<Anime> getAnimes() {
 		return animes;
